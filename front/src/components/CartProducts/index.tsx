433310a9081e00fa12facd8c01e34bd40d4cf2a5
Leandro Carvalho <dev.leandrocarvalho@gmail.com>
2023-02-21 11:03:06 -0300

    resolvendo bug pra deploy

---
+++ front/src/components/CartProducts/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+export default function CartProducts() {
+  return (
+      <main className={styles.main}>
+          <section>
+              
+          </section>
+    </main>
+  )
+}
+
